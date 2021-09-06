12da32e704ee90d1bb3e974aa2b50bc110aade06
LucasDev13 <lucaspontes140@gmail.com>
2021-09-06 15:17:58 -0300

    Criando classes para cada funcionario.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,32 @@
 fun main() {
     print("Bem vindo ao bytebank: ")
 
-    val alex = Funcionario(nome = "Alex", cpf = "111.111.111-11", salario = 1000.0)
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
 
     println("nome: ${alex.nome}")
     println("cpf: ${alex.cpf}")
     println("salario: ${alex.salario}")
     println("bonificaçao: ${alex.bonificacao()}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 123456
+    )
+
+    println("nome: ${fran.nome}")
+    println("cpf: ${fran.cpf}")
+    println("salario: ${fran.salario}")
+    println("bonificaçao: ${fran.bonificacao()}")
+
+    if (fran.autentica(123456)){
+        println("Autenticou com sucesso.")
+    }else{
+        println("Não autenticou")
+    }
 }
-
-
-
-
-
-
-
-
