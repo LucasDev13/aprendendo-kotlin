efd07a3d1172f9e335454b006678f2eda33abf3b
LucasDev13 <lucaspontes140@gmail.com>
2021-09-12 19:17:38 -0300

    Utilizando o safe call.

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,9 +1,15 @@
 import br.com.alura.bytebank.model.Endereco
 
 fun main() {
-    var enderecoNulo: Endereco? = null
-    val enderecoNaoNulo: Endereco = enderecoNulo!!
-    enderecoNaoNulo.logradouro
+    //chamada encadeada do salfe call ou um safe call combinado com um let
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua do nada")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let { endereco: Endereco ->
+//        println(it.logradouro.length)
+        println(endereco.logradouro.length)
+    }
+
+
 }
 
 
