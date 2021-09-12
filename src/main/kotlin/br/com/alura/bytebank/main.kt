885aaa3eb7b89212fd683abcfc633cb87ca7bd79
LucasDev13 <lucaspontes140@gmail.com>
2021-09-11 23:58:03 -0300

    Conhecendo e sobrescrevendo os métodos do Any.

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -2,17 +2,22 @@ import br.com.alura.bytebank.model.Endereco
 
 fun main() {
     val endereco = Endereco()
+    val enderecoNovo = endereco
+    println(endereco.equals(enderecoNovo))
+    //hashCode gera um hash que faz a identificação do objeto.
+    //então o objeto sempre vai ter esse hash independente de quantas vezes chamar
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
 
-    val objeto: Any = Any()
-    imprime(objeto)
-    imprime(Unit)
-    imprime(1)
-    imprime(1.0)
-    val teste: Any = imprime(endereco)
-    println(teste)
+    //toString é chamado sempre que precisamos imprimir um objeto
+    println(enderecoNovo.toString())
+    println(enderecoNovo.toString())
+
+    //outro forma de mostrar o nome do objeto
+    println("${endereco.javaClass}@${Integer.toHexString(endereco.hashCode())}")
 }
 
-fun imprime(valor: Any): Any{
+fun imprime(valor: Any): Any {
     println(valor)
     return valor
 }
