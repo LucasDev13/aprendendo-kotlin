83694a3688b59c6993d4d865058e4e6de9609c89
LucasDev13 <lucaspontes140@gmail.com>
2021-09-12 20:04:07 -0300

    Outros recursos do null safety.

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -4,12 +4,20 @@ fun main() {
     //chamada encadeada do salfe call ou um safe call combinado com um let
     var enderecoNulo: Endereco? = Endereco(logradouro = "Rua do nada")
     val logradouroNovo: String? = enderecoNulo?.logradouro
-    enderecoNulo?.let { endereco: Endereco ->
-//        println(it.logradouro.length)
-        println(endereco.logradouro.length)
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+//        println(endereco.logradouro.length)
+        val tamanhoCompleto: Int =
+            it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
+        println(tamanhoCompleto)
     }
 
+    teste("")
+    teste(1)
+}
 
+fun teste(valor: Any){
+    val numero: Int? = valor as? Int
 }
 
 
