cd04acf5ec20cffe55e2a00e9ba9f1c4f50a4d85
LucasDev13 <lucaspontes140@gmail.com>
2021-09-12 18:41:20 -0300

    Tipos que podem receber nulo.

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,30 +1,10 @@
-import br.com.alura.bytebank.exception.SaldoInsuficienteException
+import br.com.alura.bytebank.model.Endereco
 
 fun main() {
-    println("início main")
-//    funcao1()
-    testaComportamentosConta()
-    println("fim main")
+    var enderecoNulo: Endereco? = null
+    val enderecoNaoNulo: Endereco = enderecoNulo!!
+    enderecoNaoNulo.logradouro
 }
 
-fun funcao1(){
-    println("início funcao1")
-    try {
-        funcao2()
-    }catch (e: SaldoInsuficienteException){
-        e.printStackTrace()//vai conter a pilha de exception
-        println("foi pego a SaldoInsuficienteException.")
-    }
-    println("fim funcao1")
-}
-
-fun funcao2() {
-    println("início funcao2")
-    for (i in 1..5){
-        println(i)
-        throw SaldoInsuficienteException()
-    }
-    println("fim funcao2")
-}
 
 
