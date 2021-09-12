f5fbf744ffff239577b3cc8b742be062f5547419
LucasDev13 <lucaspontes140@gmail.com>
2021-09-11 22:30:48 -0300

    Utilizando o Any no código. part1

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,4 +1,30 @@
-import br.com.alura.bytebank.test.testaObjects
+import br.com.alura.bytebank.model.Endereco
 
 fun main() {
+    println()
+    println(1)
+    println(1.0)
+    println(true)
+    println("teste")
+
+    val endereco = Endereco()
+
+    println(endereco)
+
+    imprime()
+    imprime(1)
+    imprime(1.0)
+    imprime(endereco)
+}
+
+fun imprime(){
+}
+
+fun imprime(numero: Int){
+}
+
+fun imprime(numero: Double){
+}
+
+fun imprime(endereco: Endereco){
 }
